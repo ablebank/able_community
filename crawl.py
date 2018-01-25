99810b98fc8fa91634b3fb300106164e99ad3fe7
Wondeuk Yoon <wdyoon@kaist.ac.kr>
2018-01-26 02:26:19 +0900

    Improve parsing components.

--- crawl.py
+++ crawl.py
@@ -22,48 +22,27 @@ for link in links:
             if a['href'].find('event') > 0:
                 html_detail = urlopen(defaultURL + a['href'])
                 bsObj_detail = BeautifulSoup(html_detail, "html.parser")
-                coin = bsObj_detail.find('span', attrs={'class': 'title'}).get_text(strip=True).split(':')[0]
+                #print(bsObj_detail.find('title').get_text(strip=True).split(':')[0].rsplit(' ', 1)[1] + " (" + bsObj_detail.find('title').get_text(strip=True).split(':')[0].rsplit(' ', 1)[0] + ")")
+                coin = bsObj_detail.find('title').get_text(strip=True).split(':')[0].rsplit(' ', 1)[1] + " (" + bsObj_detail.find('title').get_text(strip=True).split(':')[0].rsplit(' ', 1)[0] + ")"
                 title = bsObj_detail.find('span', attrs={'class': 'title'}).get_text(strip=True).split(':')[1]
+                type = bsObj_detail.find('div', attrs={'class':'type'}).get_text(strip=True)
                 date = bsObj_detail.find('span', attrs={'class':'start'}).get_text(strip=True)[0:10]
                 proof = bsObj_detail.find('div', attrs={'class':'source'}).find('a', href=True)['href']
-                events.append([coin, title, date, proof])
+                events.append([coin, title, type, date, proof])
 
 
 file = open("coindar_crawl_result.txt", "w", encoding='utf-8')
 
-file.write('<style>\n')
-file.write('table {\n')
-file.write('    width:100%;\n')
-file.write('}\n')
-file.write('table, th, td {\n')
-file.write('    border: 1px solid black;\n')
-file.write('    border-collapse: collapse;\n')
-file.write('}\n')
-file.write('th, td {\n')
-file.write('    padding: 5px;\n')
-file.write('    text-align: left;\n')
-file.write('}\n')
-file.write('table#t01 tr:nth-child(even) {\n')
-file.write('    background-color: #eee;\n')
-file.write('}\n')
-file.write('table#t01 tr:nth-child(odd) {\n')
-file.write('   background-color:#fff;\n')
-file.write('}\n')
-file.write('table#t01 th {\n')
-file.write('    background-color: black;\n')
-file.write('    color: white;\n')
-file.write('}\n')
-file.write('</style>\n')
-
-file.write('<table><tr><td>COIN</td><td>TITLE</td><td>DATE</td><td>LINK</td></tr>')
+file.write('<table width="100%" border="1px solid black" border-collapse="collapse"><tr><td>코인</td><td>내용</td><td>종류</td><td>날짜</td><td>증명 링크</td></tr>')
 
 for a in events:
     file.write('<tr>\n')
     file.write('<td>' + a[0] + '</td>\n')
     file.write('<td>' + a[1] + '</td>\n')
     file.write('<td>' + a[2] + '</td>\n')
-    file.write('<td><a href=\"' + a[3] + '\">LINK!!!</a></td>\n')
-    file.write('<tr>\n')
+    file.write('<td>' + a[3] + '</td>\n')
+    file.write('<td><a href=\"' + a[4] + '\">LINK!!!</a></td>\n')
+    file.write('</tr>\n')
 
 file.write('</table>\n')
 
